7f24ff85288b9214cccab5fe34bcf1b2ed33f71f
nansiiii <nasrin.shakery8@gmail.com>
2023-06-08 19:01:33 +0330

    navbar is ready to coding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "./App.scss";
+import Navbar from "./components/navbar/Navbar.js";
 
 class App extends Component {
    render() {
       return (
          <div className="App">
-            
+            <Navbar></Navbar>
          </div>
       );
    }
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <>
+                <h1>navbar is ready to coding</h1>
+            </>
+        );
+    }
+}
+ 
+export default Navbar;
