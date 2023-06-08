5b60ed9785f59280edff71ce2fe696784b70f2b8
Behnam <behnamborhani95@gmail.com>
2023-06-08 18:41:54 +0330

    build: sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import "./App.scss";
+import Sidebar from "./components/sidebar/Sidebar";
+import Header from "./components/header/Header";
 
 class App extends Component {
    render() {
       return (
          <div className="App">
-            
+            <section className="main">
+               <Sidebar/>
+               <main>
+                  <Header/>
+               </main>
+            </section>
          </div>
       );
    }
