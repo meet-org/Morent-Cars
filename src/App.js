b165f5b3029597100a55ae1b5253540db1dfb3a7
nansiiii <nasrin.shakery8@gmail.com>
2023-06-09 12:56:03 +0330

    footer section Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./App.scss";
+import Footer from "./components/footer/Footer";
 
 class App extends Component {
    render() {
       return (
          <div className="App">
             
+
+         <Footer></Footer>
          </div>
       );
    }
